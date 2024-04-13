3c578af17b7ce3d7bdcdab8bce238b8bfb3bd1c3
ShobhitV15 <123548201+ShobhitV15@users.noreply.github.com>
2024-04-14 02:02:38 +0530

    Second Commit

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import {Container, Flex, VStack} from '@chakra-ui/react'
+import {Box, Image} from '@chakra-ui/react'
+import AuthForm from '../../components/AuthForm/AuthForm'
 
 const AuthPage = () => {
   return (
-    <div>AuthPage</div>
+    <Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px={4}>
+      <Container maxW={"container.md"} padding={0}>
+        {/**Left side */}
+        <Box display={{base:'none',md:'block'}}>   {/** base signifies for smaller screen such as phones 
+                                                    md for bigger screen */}
+         <Image src="/auth.png" h={650} alt="Phone img"/>
+        </Box>
+
+        {/**Right side */}
+        <VStack spacing={4} align={"stretch"}>
+          <AuthForm/>
+        </VStack>
+      </Container>
+    </Flex>
   )
 }
 
