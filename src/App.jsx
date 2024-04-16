9cb0a4963ce2d3b7130a4d43f49cb5548e09cdba
ShobhitV15 <123548201+ShobhitV15@users.noreply.github.com>
2024-04-16 22:24:08 +0530

    Third commit Sidebar(PageLayout) added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import AuthPage from './pages/AuthPage/AuthPage';
+import PageLayout  from './Layouts/PageLayout/PageLayout';
 
 function App() {
   return (
     <>
+    <PageLayout>   {/**Wrapping everything in pagelayout */}
     <Routes>
       <Route path='/home' element={<HomePage/>}/>
       <Route path='/auth' element={<AuthPage/>}/>
     </Routes>
+    </PageLayout>
     </>
   )
 }
--- src/Layouts/PageLayout/PageLayout.jsx
+++ src/Layouts/PageLayout/PageLayout.jsx
@@ -1,17 +1,35 @@
 import React from 'react'
 import {Flex  ,Box} from '@chakra-ui/react'
-import Sidebar from "../../components/Sidebar"
-export const PageLayout = ({children}) => {
+
+import {useLocation} from "react-router-dom"
+import Sidebar from '../../components/Sidebar/Sidebar'
+
+//instead of adding side bar to every page, we add it only once to PageLayout component and wrap the children with it.
+// This way we can have a side bar to every page except auth page.
+
+ const PageLayout = ({children}) => {
+
+    const {pathname}=useLocation() // it gives us the current path of the page
+
   return (
     <Flex>
+
+
         {/**Side bar on the left */}
-        <Box w={{base:'70px',md:"240px"}}>
-            <Sidebar/>
+        {pathname!=='/auth' ? (           // the side bar is only visible when we arn't on auth page
+        <Box w={{base:'70px',md:"240px"}}> 
+            <Sidebar></Sidebar>
         </Box>
+        ):null}
+        
+
+
         {/**The page content on the right*/}
-        <Box>
+        <Box flex={1} w={{base:"calc(100%-70px)",md:"100%-240px"}}>
             {children}
         </Box>
     </Flex>
   )
 }
+
+export default PageLayout;
