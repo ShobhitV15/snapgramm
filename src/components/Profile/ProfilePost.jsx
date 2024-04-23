53e60c88bacf9669c066b43c74d08723f88f6911
ShobhitV15 <123548201+ShobhitV15@users.noreply.github.com>
2024-04-23 23:56:48 +0530

    7th commit profile posts added to profile

--- src/components/Profile/ProfilePost.jsx
+++ src/components/Profile/ProfilePost.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { GridItem, Flex, Text, Image } from '@chakra-ui/react'
+import { AiFillHeart } from 'react-icons/ai'
+import { FaComment } from 'react-icons/fa'
 
-const ProfilePost = () => {
+const ProfilePost = ({img}) => {
   return (
-    <div>ProfilePost</div>
+    <GridItem cursor={'pointer'} borderRadius={4} overflow={'hidden'} border={'1px solid'} borderColor={'whiteAlpha.300'} position={'relative'} aspectRatio={1/1}>
+
+    <Flex opacity={0}
+    _hover={{opacity:1}} position={'absolute'} top={0} left={0} right={0} 
+    bottom={0} bg={'blackAlpha.700'} transition={'all 0.3s ease'} zIndex={1} justifyContent={'center'}>
+    
+
+    <Flex alignItems={'center'} gap={50} justifyContent={'center'}>
+
+      <Flex>
+        <AiFillHeart size={20}/>
+        <Text fontWeight={'bold'} ml={2}> 
+          7
+        </Text>
+      </Flex>
+      <Flex>
+        <FaComment size={20}/>
+        <Text fontWeight={'bold'} ml={2}> 
+          7
+        </Text>
+      </Flex>
+
+    </Flex>
+    </Flex>
+    <Image src={img} alt='profile posts' w={'100%'} h={'100%'} objectFit={'cover'}/>
+    </GridItem>
   )
 }
 
--- src/components/Profile/ProfilePosts.jsx
+++ src/components/Profile/ProfilePosts.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import ProfilePost from './ProfilePost'
+import { Grid,VStack,Skeleton,Box } from '@chakra-ui/react'
 
 const ProfilePosts = () => {
+  const[isLoading,setLoading]=useState(true)
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      setLoading(false)
+    },2000)
+  },[])
   return (
-    <div>ProfilePosts</div>
+    <Grid templateColumns={{sm:"repeat(1,1fr)",md:"repeat(3,1fr)"}} gap={1} columnGap={1}>
+      {isLoading&&[1,2,3,4,5,6].map((_,idx)=>(
+        <VStack key={idx} alignItems={'flex-start'} gap={4}> 
+        <Skeleton>
+          <Box h={'300px'}>
+            Contents Wrapped
+          </Box>
+
+        </Skeleton>
+        </VStack>
+      ))}
+
+      {!isLoading &&(
+        <>
+        <ProfilePost img='img1.png'/>
+        <ProfilePost img='img2.png'/>
+        <ProfilePost img='img3.png'/>
+        <ProfilePost img='img4.png'/>
+
+        
+        </>
+      )}
+    </Grid>
   )
 }
 
